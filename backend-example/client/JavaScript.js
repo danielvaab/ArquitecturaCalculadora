01c37a5c1ba3965ebbf52fb25f8aa31933da8ed0
danielvaab <danielvaab@unisabana.edu.co>
2020-11-17 19:20:32 -0500

    Backend

--- backend-example/client/JavaScript.js
+++ backend-example/client/JavaScript.js
@@ -1,70 +1,146 @@
-document.getElementById('valor3').style.display = 'none';
-document.getElementById('TextT3').style.display = 'none';
+document.getElementById("valor3").style.display = "none";
+document.getElementById("TextT3").style.display = "none";
 //Triangulo
 function Borrar() {
   document.getElementById("valor1").value = "";
   document.getElementById("valor2").value = "";
   document.getElementById("valor3").value = "";
-  document.getElementById("ResultT").value= "";
+  document.getElementById("ResultT").value = "";
 }
 function AreaT() {
   document.getElementById("TextT1").innerHTML = "Base: ";
   document.getElementById("TextT2").innerHTML = "Altura: ";
 
-
-  document.getElementById('TextT3').style.display = 'none';
-  document.getElementById('valor3').style.display = 'none';
+  document.getElementById("TextT3").style.display = "none";
+  document.getElementById("valor3").style.display = "none";
 }
 function PerimetroT() {
   document.getElementById("TextT1").innerHTML = "Lado 1: ";
   document.getElementById("TextT2").innerHTML = "Lado 2: ";
   document.getElementById("TextT3").innerHTML = "Base: ";
-  
 
-  document.getElementById('valor3').style.display = 'inline';
-  document.getElementById('TextT3').style.display = 'inline';
+  document.getElementById("valor3").style.display = "inline";
+  document.getElementById("TextT3").style.display = "inline";
 }
 function CalcularT() {
   if (document.getElementById("TextT1").innerText == "Base: ") {
-    let a = parseInt(document.getElementById("valor1").value);
-    let b = parseInt(document.getElementById("valor2").value);
-    document.getElementById("ResultT").value = (a * b) / 2;
+    let b = parseInt(document.getElementById("valor1").value);
+    let h = parseInt(document.getElementById("valor2").value);
+    let data = { b, h };
+    fetch("/calcularTrianguloA", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error))
+      .then(
+        (response) =>
+          (document.getElementById("ResultT").value = response.respuesta)
+      );
   } else {
     if (document.getElementById("TextT1").innerText == "Lado 1: ") {
       let a = parseInt(document.getElementById("valor1").value);
       let b = parseInt(document.getElementById("valor2").value);
       let c = parseInt(document.getElementById("valor3").value);
-      document.getElementById("ResultT").value = c + a + b;
+      let data = { a, b, c };
+      fetch("/calcularTrianguloP", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then(
+          (response) =>
+            (document.getElementById("ResultT").value = response.respuesta)
+        );
     }
   }
 }
-//Rectangulo
+
+//----------------------------------Rectangulo
 function BorrarR() {
   document.getElementById("valor4").value = "";
   document.getElementById("valor5").value = "";
-  document.getElementById("ResultR").value= "";
+  document.getElementById("ResultR").value = "";
 }
 
 function PerimetroR() {
-    let a = parseInt(document.getElementById("valor4").value);
-    let b = parseInt(document.getElementById("valor5").value);
-    document.getElementById("ResultR").value = (a * 2 + b* 2);
+  let a = parseInt(document.getElementById("valor4").value);
+  let b = parseInt(document.getElementById("valor5").value);
+  let data = { a, b };
+  fetch("/calcularRectanguloP", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then(
+      (response) =>
+        (document.getElementById("ResultR").value = response.respuesta)
+    );
 }
+
 function AreaR() {
   let a = parseInt(document.getElementById("valor4").value);
   let b = parseInt(document.getElementById("valor5").value);
-  document.getElementById("ResultR").value = (a * b);
+  let data = { a, b };
+  fetch("/calcularRectanguloA", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then(
+      (response) =>
+        (document.getElementById("ResultR").value = response.respuesta)
+    );
 }
-//Circulo
-function BorrarC(){
+//----------------------------------Circulo
+function BorrarC() {
   document.getElementById("valor6").value = "";
-  document.getElementById("ResultC").value= "";
+  document.getElementById("ResultC").value = "";
 }
 function PerimetroC() {
-  let a = parseInt(document.getElementById("valor6").value);
-  document.getElementById("ResultC").value = (2*a*Math.PI);
+  let r = parseInt(document.getElementById("valor6").value);
+  fetch("/calcularCirculoP", {
+    method: "POST",
+    body: JSON.stringify({ r }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then(
+      (response) =>
+        (document.getElementById("ResultC").value = response.respuesta)
+    );
 }
 function AreaC() {
-let a = parseInt(document.getElementById("valor6").value);
-document.getElementById("ResultC").value = (Math.pow(a,2)*Math.PI);
+  let r = parseInt(document.getElementById("valor6").value);
+  fetch("/calcularCirculoA", {
+    method: "POST",
+    body: JSON.stringify({ r }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then(
+      (response) =>
+        (document.getElementById("ResultC").value = response.respuesta)
+    );
 }
--- backend-example/server/routes/main.routes.js
+++ backend-example/server/routes/main.routes.js
@@ -9,18 +9,37 @@ router.get("/posts",async (req,res) =>{
 
 router.post('/posts',async (req,res)=>{
     let {name,category} = req.body;
-    
     res.status(200).json({newId})
 })
 
-router.put('/posts',async (req,res)=>{
-    let {id,name} = req.body;
-    res.status(200).end('OK')
+// Calcular triangulo
+router.post('/calcularTrianguloA',async (req,res)=>{
+    let {b,h} = req.body;
+    res.status(200).json({respuesta: b*h/2})
 })
-
-router.delete('/posts/:id',async (req,res)=>{
-    let {id} = req.params;
-    res.status(200).end('OK')
+router.post('/calcularTrianguloP',async (req,res)=>{
+    let {a,b,c} = req.body;
+    res.status(200).json({respuesta: a+b+c})
+})
+// Calcular rectangulo
+router.post('/calcularRectanguloA',async (req,res)=>{
+    let {a,b} = req.body;
+    res.status(200).json({respuesta: a*b})
+})
+router.post('/calcularRectanguloP',async (req,res)=>{
+    let {a,b} = req.body;
+    res.status(200).json({respuesta: 2*a+2*b})
+})
+// Calcular circulo
+router.post('/calcularCirculoA',async (req,res)=>{
+    let {r} = req.body;
+    console.log(r);
+    res.status(200).json({respuesta: Math.PI * r * r})
+})
+router.post('/calcularCirculoP',async (req,res)=>{
+    let {r} = req.body;
+    console.log(r);
+    res.status(200).json({respuesta: 2 * Math.PI * r})
 })
 
 module.exports = router;
